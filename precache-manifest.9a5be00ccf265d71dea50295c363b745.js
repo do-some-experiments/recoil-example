3d9ed5e0056e7fb02133b9ac062835166cbb7d7c
Jakub Biesiada <biesiadajakub@icloud.com>
2020-05-15 20:05:33 +0200

    Updates

---
+++ precache-manifest.9a5be00ccf265d71dea50295c363b745.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e293a6c6c9219c08ef212649809e4cae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0e35c1e3b02165c1261",
+    "url": "./static/js/2.75fe090c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.75fe090c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c920ad3c0fb4f1bd6f1",
+    "url": "./static/js/main.b69722b2.chunk.js"
+  },
+  {
+    "revision": "c123feb8d00c85a6a27a",
+    "url": "./static/js/runtime-main.f9c7bde5.js"
+  }
+]);
